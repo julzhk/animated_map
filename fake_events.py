6e45f50e34b1c1136a689e08a3b69d2ebccbad15
Julian Harley <julian.harley@gmail.com>
2014-04-22 20:07:21 +0100

    fake data

--- fake_events.py
+++ fake_events.py
@@ -9,7 +9,7 @@ def rnd(seed):
 for i in range(0,250):
     ev = i % 4 + 1
     tm = rnd(i)
-    print 'tl.insert(TweenLite.to("#event%(ev)s", 0, {scale:0,top:%(x)s,left:%(y)s,autoAlpha:0}),%(tm)s)' % {'ev':ev,'x':rnd(0)*60,'y':rnd(0)*50,'tm':tm} 
+    print 'tl.insert(TweenLite.to("#event%(ev)s", 0, {scale:1,top:%(x)s,left:%(y)s,autoAlpha:0}),%(tm)s)' % {'ev':ev,'x':int(rnd(0)*60),'y':int(rnd(0)*50),'tm':tm-0.01} 
     
     print 'tl.insert(TweenLite.to("#event%(ev)s", .5, {autoAlpha:1, delay:0,scale:%(scale)s}),%(tm)s)' % {'ev':ev,'scale':rnd(0),'tm':tm},
     print '.to("#event%(ev)s", 0.5, {autoAlpha:0},%(td)s);' % {'ev':ev,'td':tm+2}
